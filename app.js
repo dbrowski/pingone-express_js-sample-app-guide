d124adf82970100c5a0769c4cf3a31bd5ddab0c3
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-07-18 21:02:14 -0500

    add in dotenv to read from .env file create variables for the env var's

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Allows us to read values from ".env" file.
+require("dotenv").config();
+
+/**
+ * First, copy the .env.EXAMPLE file to a .env file.
+ * Then, fill in your values in that file.
+ */
+// PingOne auth base url
+const authBasePath = process.env.PINGONE_AUTH_BASE_URL;
+// PingOne environment ID
+const envID = process.env.PINGONE_ENV_ID;
+// PingOne client ID of the app connection
+const clientID = process.env.PINGONE_CLIENT_ID;
+// PingOne client secret of the app connection
+const clientSecret = process.env.PINGONE_CLIENT_SECRET;
+// Express app base url
+const appBasePath = process.env.APP_BASE_URL;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
