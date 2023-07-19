303ec3b19d5fdc3775541e14989ac253363f7967
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-07-18 20:41:48 -0500

    starting point - Express's hello world example

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * This is the Hello world example from Express
+ * https://expressjs.com/en/starter/hello-world.html
+ */
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
